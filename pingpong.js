7a4cdf2dff1f457209945230ec6e1fab395b9988
Batman034 <pereira.marques.matheus@escola.pr.gov.br>
2024-06-27 17:41:28 -0300

    cria a bolinha

--- pingpong.js
+++ pingpong.js
@@ -1,10 +1,15 @@
 function Setup(){
     //aqui vou criar minha "mesa"
-    createCanvas(600,400) ;
+    createCanvas(600,400);
 }
 
 function draw(){
     //aqui vou por a cor da "mesa"
-    background(255,255,255) ;
-    
+    background(255,255,255);
+
+    criabolinha(xbolinha, ybolinha, tamboliha);
+}
+// função bolinha
+function criabolinha(xbolinha, ybolinha, tambolinha){
+    circle (xbolinha, ybolinha, tamboliha);
 }
