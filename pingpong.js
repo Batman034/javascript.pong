6d02e287114a90835915e4b75872b67b528b9be9
Batman034 <pereira.marques.matheus@escola.pr.gov.br>
2024-08-01 16:00:36 -0300

    .

--- pingpong.js
+++ pingpong.js
@@ -1,11 +1,17 @@
 //aqui eu vou criar a bolinha
 let xBolinha = 300;
 let yBolinha = 200;
-let tamBolinha = 25;
+let raioBolinha = tamBolinha = 25;
 
 //config velocidade bolinha
 let xvelocidadeBolinha = 6;
 let yvelocidadeBolinha = 6;
+
+//criar raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let larguraRaquete = 10;
+let alturaraquete = 90;
 
 function setup(){
     //aqui vou criar minha "mesa"
@@ -21,6 +27,9 @@ function draw(){
     criaBolinha(xBolinha, yBolinha, tamBolinha);
     //chamando a função mov bolinha
     moveBolinha();
+    criaRaquete(xRaquete, yRaquete, larguraRaquete, alturaraquete);
+    movimentaRaquete();
+    colideRaquete();
     //chamando a borda
     Borda();
 }
@@ -28,6 +37,7 @@ function draw(){
 //função bolinha
 function criaBolinha(xBolinha, yBolinha,tamBolinha){
     circle (xBolinha,yBolinha,tamBolinha);
+    fill("red");
 }
 
 //função move bolinha
@@ -42,5 +52,33 @@ function Borda(){
     }
     if (yBolinha > height || yBolinha < 0){
         yvelocidadeBolinha *= -1;
+    }
+}
+
+function criaRaquete(xRaquete, yRaquete, alturaraquete, larguraRaquete){
+    fill("blue");
+    rect(xRaquete, yRaquete, larguraRaquete, alturaraquete);
+}
+//funçao responsavel por criar o retangulo que representa a raquete
+function criaRaquete (xRaquete, yRaquete, larguraRaquete, alturaraquete){
+    fill("blue");
+    rect(xRaquete, yRaquete, larguraRaquete, alturaraquete);
+}
+//função responsavel por movimentar a raquete
+function movimentaRaquete (){
+
+    if(keyIsDown(UP_ARROW)){
+        yRaquete -=10;
+    }
+    if(keyIsDown(DOWN_ARROW)){
+        yRaquete +=10;
+    }
+}
+//função responsavel por quando a bolinha bater na raquete, retornar em direção contraria.
+function colideRaquete(){
+
+    if(xBolinha - raioBolinha < xRaquete + larguraRaquete && yBolinha - raioBolinha < yRaquete + alturaraquete && yBolinha + raioBolinha > yRaquete){
+        xvelocidadeBolinha *=-1;
+
     }
 }
